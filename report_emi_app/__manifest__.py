6e7bfda070fc8a96253bbe862e8c2da111718a9e
I am Neeraj <neerajkrishnan82@gmail.com>
2022-05-19 13:05:08 +0530

    this is the new project of client

---
+++ report_emi_app/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'EMI Report',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'It is custom Addon which generate Report',
+    'description': "",
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
